bd89bc21ea44fb8a7f8ec5554f2cfd2cbcc3de1c
Tomas-crypto14 <tomasignaciocapalbo@gmail.com>
2025-06-03 10:02:49 +0200

    Pokedex Actualizada

--- src/components/Organisms/PokemonList.jsx
+++ src/components/Organisms/PokemonList.jsx
@@ -1,12 +1,21 @@
 import {useEffect} from "react";
+import axios from "axios";
 const PokemonList = () => {
+    const [pokemons, setPokemons] = useState([]);
     useEffect(() => {
-        fetch(`https://pokeapi.co/api/v2/pokemon/`)
-        .then((res) => res.json())
-        .then((data) => console.log(data))
+        axios.get(`https://pokeapi.co/api/v2/pokemon/`)
+        .then((data) => data.data)
+        .then((pokemonList) => {
+            setPokemons(pokemonList)
+        })
     }, []);
   return (
-    <div>PokemonList</div>
+    <div>
+        <h2>PokemonList</h2>
+        {pokemons.map((pokemon) => {
+            return <p>{pokemon.name}</p>
+        })}
+    </div>
     )
 }
 
